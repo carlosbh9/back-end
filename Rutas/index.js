01d7838b701bbbb5b5c289a721cce764f49fc7ed
Carlos <017100872b@uandina.edu.pe>
2024-07-01 12:11:43 -0500

    routing_vw2

--- Rutas/index.js
+++ Rutas/index.js
@@ -1,14 +1,28 @@
 const exs = require("express");
 
-//const HotelsRoutes = require ("./Hotel/hotel.route")
-//const TrainRoutes = require ("./Train/train.route")
+const entranceRoutes = require ("./Entrance/entrance.route")
+const expeditionRoute = require("./Expedition/expedition.route")
+const experienceRoute = require("./Experience/experience.route")
+const guideRoute = require("./Guide/guide.route")
+const hotelRoute = require("./Hotel/hotel.route")
+const operatorRoute = require("./Operator/operator.route")
 const restaurantRoutes = require ("./Restaurant/restaurant.route")
+const trainRoutes = require ("./Train/train.route")
+const transportRoute = require("./Transport/transport.route")
+
 
 function routes(app){
     const route = exs.Router();
     app.use("/api",route);
-    app.use('/api/restaurants', restaurantRoutes);
-    //route.use("/train",TrainRoute);
+    route.use("/entrances",entranceRoutes);
+    route.use("/expeditions",expeditionRoute);
+    route.use("/experiences",experienceRoute);
+    route.use("/guides",guideRoute);
+    route.use("/hotels",hotelRoute);
+    route.use("/operators",operatorRoute);
+    route.use('/restaurants', restaurantRoutes);
+    route.use("/trains",trainRoutes);
+    route.use("/transports",transportRoute);
 }
 
 module.exports = routes;
