15736ed29fc47eb4529dbc9a55e8538819802306
Carlos <017100872b@uandina.edu.pe>
2024-07-15 11:59:06 -0500

    finalv.1

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const exs = require("express");
 const routes = require('./src/Rutas');
 const connectDB = require('./db/db.js');
 //require('./src/models/Restaurant.schema.js')
-//const cors = require('cors')
+const cors = require('cors')
 //const { use } = require("./Rutas/Lotes/Lotes.route");
 
 //const {mostrarError, manejarError, boomManejaError}= require('./middlewares/error.middleware');
 const apk = exs();
 const puerto = 3000;
 apk.use(exs.json());
-//apk.use(cors());
+apk.use(cors());
 connectDB();
 routes(apk);
 
