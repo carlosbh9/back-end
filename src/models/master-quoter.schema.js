c3dbcad302fd100cf0cfb838e658b109cef0abe6
Carlos <017100872b@uandina.edu.pe>
2024-10-21 17:07:08 -0500

    21/10

--- src/models/master-quoter.schema.js
+++ src/models/master-quoter.schema.js
@@ -2,21 +2,25 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema
 
 
+const servicesShema = new Schema({
+    type_service:{type: String},
+    name_service:{type: String },
+    service_id: { type: Schema.Types.ObjectId, required: true },  // ID del servicio referenciado
+    service_type: { type: String, enum: ['entrance', 'restaurant', 'operator','expeditions','experience','guides','hotel','train','transport'], required: true },  // Tipo del servicio
+    operator_service_id: { type: Schema.Types.ObjectId, required: false } , // ID del servicio específico dentro de un operador (si es 'operator')
+    train_service_id: {type: Schema.Types.ObjectId, required: false },
+})
+
+const daysSchema = new Schema({
+    
+    services: [servicesShema]
+}) 
 
 const masterQuoterSchema = new Schema({
     name: { type: String, required: true },// Nombre de la opción programada
     days: {type: Number} ,
-    destinations: {type:String} ,
-    services: [
-        {
-            name_service:{type: String },
-            service_id: { type: Schema.Types.ObjectId, required: true },  // ID del servicio referenciado
-            service_type: { type: String, enum: ['entrance', 'restaurant', 'operator','expeditions','experience','guides','hotel','train','transport'], required: true },  // Tipo del servicio
-            operator_service_id: { type: Schema.Types.ObjectId, required: false } , // ID del servicio específico dentro de un operador (si es 'operator')
-            hotels_service_id: {type: Schema.Types.ObjectId, required: false},
-            train_service_id: {type: Schema.Types.ObjectId, required: false }
-        }
-    ]
+    destinations: {type:String},
+    day: [daysSchema]
 }, { timestamps: true });
 
 module.exports = mongoose.model('MasterQuoter', masterQuoterSchema);
